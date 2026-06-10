c8489f35125334c7ee65a05c5736b7aa920de7de
Victorzhan.Hwacom <victorzhan.hwacom@gmail.com>
2026-06-10 14:12:00 +0800

    Update

---
+++ Runtime/Prefabs/Charts/ProgressBar/CircleProgressBar.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class CircleProgressBar : MonoBehaviour
+{
+  #region Fields
+ /*  [Foldout()] public UnityEvent<float> onProgressChanged;
+  [] public UnityEvent<float> onProgressChanged01;
+  [] public UnityEvent onComplete; */
+  [SerializeField] private Image progressBar;
+    #endregion
+
+    public void SetProgress(float progress)
+    {
+        // progressBar.fillAmount = progress;
+    }
+
+    public void SetProgress01(float progress01)
+    {
+        // progressBar.fillAmount = Mathf.Clamp01(progress);
+    }
+
+    public void SetMaxValue(float maxValue)
+    {
+    }
+    
+    public void SetValue(float value)
+    {
+    }
+    
+
+}
