739f1d89ef3ec67609f18100a2aa2bc78231fc89
Victorzhan.Hwacom <victorzhan.hwacom@gmail.com>
2026-07-20 15:38:32 +0800

    Update

---
+++ Runtime/Scripts/WebUtils/WebGLBridge_MainMenu.cs
@@ -0,0 +1,31 @@
+using NaughtyAttributes;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace VzDev.WebGLUtils
+{
+        /// <summary>
+        /// 接收JS端訊息 (For主選單控制)
+        /// </summary>
+        public class WebGLBridge_MainMenu : WebGLBridge
+        {
+                [field: SerializeField, ReadOnly]
+                public string MainMenuIndex { get; private set; } = string.Empty;
+
+                [Foldout("[Events-Custom]")] public UnityEvent<int> OnMainMenuChanged;
+
+                public void SetMainMenu(string mainMenuIndex)
+                {
+                        MainMenuIndex = mainMenuIndex;
+                        if (int.TryParse(MainMenuIndex, out int index))
+                        {
+                                Debug.Log($"[WebGLBridge_MainMenu] MainMenuIndex 已更新為: {index}");
+                                OnMainMenuChanged?.Invoke(index);
+                        }
+                        else
+                        {
+                                Debug.LogWarning($"[WebGLBridge_MainMenu] 無法解析 MainMenuIndex: {MainMenuIndex} 為整數");
+                        }
+                }
+        }
+}
