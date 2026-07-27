0ba2a89733a7dd159441f7d016441b07bc44041d
Victorzhan.Hwacom <victorzhan.hwacom@gmail.com>
2026-07-27 10:44:59 +0800

    Update

--- Runtime/Prefabs/DCIM/點位標籤/點位標籤 - 環控/LeakLabelGetter.cs
+++ Runtime/Prefabs/DCIM/點位標籤/點位標籤 - 環控/LeakLabelGetter.cs
@@ -1,6 +1,7 @@
+using System.Linq;
 using UnityEngine;
+using VzDev.StringUtils;
 using VzDev.ToolUtils;
-using VzDev.UnityAPI.Extensions;
 
 /// <summary>
 /// 漏水帶標籤獲取器 - 從模型名稱中提取漏水帶的索引作為標籤。
@@ -9,7 +10,8 @@ public class LeakLabelGetter : MonoBehaviour, IPointTagLabelGetter
 {
     public string GetLabel(Transform targetModel)
     {
-        string index = "LK-" +targetModel.name.GetStringBetweenMarks("(", ")");
-        return index;
+        string deviceCode = StringHelper.GetStringFromInterval(targetModel.name, "[", "]");
+        string raw = deviceCode.Split(":").LastOrDefault();
+        return raw.Split("+").LastOrDefault();
     }
 }
