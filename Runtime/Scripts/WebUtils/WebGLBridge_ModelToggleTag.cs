1bd1cca2918fb0d2f630cb40f91b2c38bae39712
Victorzhan.Hwacom <victorzhan.hwacom@gmail.com>
2026-07-30 18:01:22 +0800

    Update

--- Runtime/Scripts/WebUtils/WebGLBridge_ModelToggleTag.cs
+++ Runtime/Scripts/WebUtils/WebGLBridge_ModelToggleTag.cs
@@ -20,9 +20,13 @@ namespace VzDev.WebGLUtils
                 [Foldout("[Components]"), SerializeField] private ModelToggleBindingGenerator modelToggleCCTV, modelToggleDoor;
                 [Foldout("[Components]"), SerializeField] private ModelToggleBindingGenerator[] modelToggleAcSystem;
 
-                public void SetCctvToggleOn(string deviceCode, string isOn) => FindObjectByDeviceCode(modelToggleCCTV.ModelToggles, deviceCode, isOn);
-                public void SetDoorToggleOn(string deviceCode, string isOn) => FindObjectByDeviceCode(modelToggleDoor.ModelToggles, deviceCode, isOn);
-                public void SetAcSystemToggleOn(string deviceCode, string isOn)
+                public void SetCctvToggleOn(string deviceCode) => FindObjectByDeviceCode(modelToggleCCTV.ModelToggles, deviceCode, true);
+                public void SetCctvToggleOff(string deviceCode) => FindObjectByDeviceCode(modelToggleCCTV.ModelToggles, deviceCode, false);
+                public void SetDoorToggleOn(string deviceCode) => FindObjectByDeviceCode(modelToggleDoor.ModelToggles, deviceCode, true);
+                public void SetDoorToggleOff(string deviceCode) => FindObjectByDeviceCode(modelToggleDoor.ModelToggles, deviceCode, false);
+                public void SetAcSystemToggleOn(string deviceCode) => SetAcSystemToggleOn(deviceCode, true);
+                public void SetAcSystemToggleOff(string deviceCode) => SetAcSystemToggleOn(deviceCode, false);
+                private void SetAcSystemToggleOn(string deviceCode, bool isOn)
                 {
                         if (modelToggleBindingsAcSystem == null || modelToggleBindingsAcSystem.Length == 0)
                         {
@@ -38,7 +42,7 @@ namespace VzDev.WebGLUtils
                 /// <summary>
                 /// 透過DeviceCode找到對應的模型，並觸發事件傳遞該模型的Transform。
                 /// </summary>
-                private void FindObjectByDeviceCode(ModelToggleBinding[] targetList, string deviceCode, string isOn)
+                private void FindObjectByDeviceCode(ModelToggleBinding[] targetList, string deviceCode, bool isOn)
                 {
                         ReceiveDeviceCode = deviceCode;
                         foundModelByDeviceCode = Array.Find(targetList, modelToggle => modelToggle.TargetModel.name.Contains(deviceCode, StringComparison.OrdinalIgnoreCase));
@@ -50,20 +54,8 @@ namespace VzDev.WebGLUtils
                         }
                         else
                         {
-                                foundModelByDeviceCode.GetComponent<ModelToggleBinding>().ToggleItem.isOn = bool.TryParse(isOn, out bool result) ? result : false;
+                                foundModelByDeviceCode.GetComponent<ModelToggleBinding>().ToggleItem.isOn = isOn;
                         }
                 }
-
-
-                public string testDeviceCode = string.Empty;
-                public string boolString = "false";
-                [Button]
-                private void Test_CCTV() => SetCctvToggleOn(testDeviceCode, boolString);
-
-                [Button]
-                private void Test_Door() => SetDoorToggleOn(testDeviceCode, boolString);
-                [Button]
-                private void Test_AC() => SetAcSystemToggleOn(testDeviceCode, boolString);
-
         }
 }
